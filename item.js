263be28696274210d079f6eb8af5ce9cf7fa8ba2
Shawn Cannon <scanno16@ford.com>
2022-04-17 12:18:52 -0600

    create wepon list

---
+++ item.js
@@ -0,0 +1,26 @@
+const pullWepons = "https://eldenring.fanapis.com/api/weapons?limit=100";
+const $app = document.querySelector(".box")
+fetch(pullWepons)
+    .then((repsonse) => repsonse.json())
+    .then((response) => {
+        const wepons = response.data;
+        return Promise.all(wepons);
+    })
+    .then((wepons) => {
+        wepons
+            .map((wepon) => {
+                const $weponBox = document.createElement("div");
+                $weponBox.innerHTML = `
+   <h3>${wepon.name}<h3>
+   <img src="${wepon.image}" />
+   <p>${wepon.description} </p>
+   <p> Scales with:
+    ${wepon.scalesWith.map(tool => tool.name)} 
+   </p> 
+   `;
+                return $weponBox;
+            })
+            .forEach(($weponBox) => {
+                $app.append($weponBox);
+            });
+    });
