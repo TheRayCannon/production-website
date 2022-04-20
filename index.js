4337f242fc6147534fb6dbc055f884daba982183
Shawn Cannon <scanno16@ford.com>
2022-04-20 00:30:11 -0600

    combine fetches

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("form");
 const pullSourcery = "https://eldenring.fanapis.com/api/sorceries?limit=10";
+const pullInc = "https://eldenring.fanapis.com/api/incantations?limit=10";
+const pullWepons = "https://eldenring.fanapis.com/api/weapons?limit=5";
 const $box = document.querySelector("#box");
 
 
@@ -11,6 +13,7 @@ form.addEventListener("submit", (event) => {
     const int = formData.get("int");
     const faith = formData.get("faith");
     const arcane = formData.get("arcane");
+
     fetch(pullSourcery)
         .then((response) => response.json())
         .then((response) => {
@@ -18,7 +21,6 @@ form.addEventListener("submit", (event) => {
             return Promise.all(spells);
         })
         .then((spells) => {
-
             spells
                 .filter((spell) => spell.requires !== null)
                 .filter((spell) => {
@@ -52,5 +54,81 @@ form.addEventListener("submit", (event) => {
                     $box.append($spellBox);
                 });
         })
+    fetch(pullInc)
+        .then(response => response.json())
+        .then(response => {
+            const inc = response.data
+            return Promise.all(inc)
+        })
+        .then((incants) => {
+            incants
+                .filter((incant) => {
+                    const intIndex = 0;
+                    const faithIndex = 1;
+                    const arcaneIndex = 2;
+                    return (
+                        incant.requires[intIndex].amount <= +int &&
+                        incant.requires[faithIndex].amount <= +faith &&
+                        incant.requires[arcaneIndex].amount <= +arcane
+                    );
+                })
+                .map((incant) => {
+                    const $incantBox = document.createElement("div");
+                    $incantBox.classList.add("incantBox");
+                    const req = incant.requires.map((object) => {
+                        return `  ${object.name}: ${object.amount}`;
+                    });
+                    $incantBox.innerHTML = `
+                    <h1>Spall Name: ${incant.name}<h1/>
+                    <img src="${incant.image}" class="spellImg"/>
+                    <p>${incant.description}</p>
+                    <p>Type: ${incant.type}</p>
+                    <p>Cost: ${incant.cost} FP</p>
+                    <p>Effetcs:${incant.effects}</p>
+                    <p>Requirements: ${req}<p>
+                    `;
+                    return $incantBox;
+                })
+                .forEach(($incantBox) => {
+                    $box.append($incantBox);
+                });
+        })
 
+    fetch(pullWepons)
+        .then((repsonse) => repsonse.json())
+        .then((response) => {
+            const wepons = response.data;
+            return Promise.all(wepons);
+        })
+        .then((wepons) => {
+            wepons
+                .filter((wepon) => {
+                    const strIndex = 0;
+                    const dexIndex = 1;
+                    return (
+                        wepon.requiredAttributes[strIndex].amount <= +strength &&
+                        wepon.requiredAttributes[dexIndex].amount <= +dex
+                    );
+                })
+                .map((wepon) => {
+                    const $weponBox = document.createElement("div");
+                    $weponBox.classList.add("weponBox")
+                    const req = wepon.requiredAttributes
+                        .map(
+                            object => { return ` ${object.name}: ${object.amount}` })
+                    $weponBox.innerHTML = `
+       <h3>${wepon.name}<h3>
+       <img src="${wepon.image}" />
+       <p>${wepon.description} </p>
+       <p> Scales with:
+        ${wepon.scalesWith.map(tool => tool.name)} 
+       </p> 
+       <p> Requirments: ${req}</p>
+       `;
+                    return $weponBox;
+                })
+                .forEach(($weponBox) => {
+                    $box.append($weponBox);
+                });
+        });
 });
